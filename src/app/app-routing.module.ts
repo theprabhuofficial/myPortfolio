1fca9c98cbbda1ab6534f6bab6680e37e9dbaaad
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2024-05-11 13:19:13 +0530

    updated experience section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,10 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ContainerComponent } from './components/container/container.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
+ 
   {
     path: '',
    component:ContainerComponent
@@ -20,23 +21,23 @@ const routes: Routes = [
   children: [
     {
       path: 'education',
-      component: EducationComponent // or just a placeholder if it's part of HomeComponent
+      component: EducationComponent 
     },
     {
       path: 'skill',
-      component: SkilsComponent // or just a placeholder if it's part of HomeComponent
+      component: SkilsComponent 
     },
     {
       path: 'project',
-      component: ProjectsComponent // or just a placeholder if it's part of HomeComponent
+      component: ProjectsComponent 
     },
     {
       path: 'experience',
-      component: ExperienceComponent // or just a placeholder if it's part of HomeComponent
+      component: ExperienceComponent 
     },
     {
       path: 'contact',
-      component: ContactComponent // or just a placeholder if it's part of HomeComponent
+      component: ContactComponent 
     },
   ]
 }
@@ -44,7 +45,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),HttpClientModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
